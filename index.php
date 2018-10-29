3faf4a9c94aad0ef79d321d2499420cb6df1ad57
BibianaDavila <00265176@ufrgs.br>
2018-10-28 23:20:08 -0300

    first
    estrutura inicial

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- título site -->
+    <title>Jornalismo e democracia</title>
+
+    <!--  jquery + d3 (gráficos) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.5/d3.min.js"></script>
+
+    <!-- fonts google + font awesome (ícones) -->
+    <link href="https://fonts.googleapis.com/css?family=Noto+Serif+JP|Raleway" rel="stylesheet">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+	<!-- estilos (css+bootstrap) -->
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="shortcut icon" href="favicon.ico" />
+
+	<!-- 
+	//////////////////////////////////////////////////////
+
+	Projeto para ciberjornalismo II - FABICO/UFRGS
+	Desenvolvido por Bibiana C. Davila
+
+	Outubro/2018
+	
+	//////////////////////////////////////////////////////
+	 -->
+</head>
+<body>
+
+	<!-- início -->
+	<section id="home">
+		<div class="container-fluid">
+	  		<div class="row">
+	  			<div class="col-md-10 offset-md-1">
+	  				<h1>Jornalismo&<br/>democracia</h1>
+	  				<div class="divider"></div>
+
+	  				<p>
+	  					Wafer muffin muffin lemon drops cotton candy cotton candy liquorice gingerbread gummies. Wafer jelly-o donut topping. Donut liquorice croissant icing marzipan muffin jelly lemon drops chocolate bar. Brownie danish cake bear claw jujubes caramels sweet roll dessert ice cream. Soufflé cake gummi bears. Toffee brownie brownie macaroon sweet. Candy danish croissant sweet chocolate bar lemon drops icing gummi bears croissant. Chocolate gingerbread tootsie roll bonbon danish. Topping jelly beans jelly-o pie cheesecake. Carrot cake wafer cake biscuit macaroon sweet sweet roll. Cake carrot cake pudding jelly. Cupcake muffin cookie. Sweet fruitcake chocolate bar. Sweet chocolate bar ice cream.
+	  				</p>
+	  				
+	  			</div>
+	  		</div>
+	  	</div>
+	</section>
+  </body>
+</html>
---
+++ js/main.js
@@ -0,0 +1,109 @@
+var rotation = 0;
+var rotation1 = 0;
+
+jQuery.fn.rotate = function(degrees) {
+    $(this).css({'-webkit-transform' : 'rotate('+ degrees +'deg)',
+                 '-moz-transform' : 'rotate('+ degrees +'deg)',
+                 '-ms-transform' : 'rotate('+ degrees +'deg)',
+                 'transform' : 'rotate('+ degrees +'deg)'});
+    return $(this);
+};
+function animation(){
+
+
+	var scroll = $(document).scrollTop();
+	
+	if($(window).width()>480){
+		$('.overlay-bg').css('background-position-y',scroll/4);
+		$('.overlay-bg.dois').css('background-position-y',scroll/3);
+	}
+	var height = 100-scroll/3;
+	//$('#home article').css('min-height',height+'vh');
+
+	var sec1 = $('#prim').offset().top;
+	var sec2 = $('#seg').offset().top;
+	var sec3 = $('#ter').offset().top;
+	var sec4 = $('#sex').offset().top;
+	var sec5 = $('#qui').offset().top;
+	var sec6 = $('#quar').offset().top;
+	var sec7 = $('#set').offset().top;
+
+	/* home */
+	if(scroll<sec1){
+		
+	}
+	/* primeiro */
+	else if(scroll<sec2){
+		$('.earth').css('marginLeft',scroll/3);
+	}
+	/* segundo */
+	else if(scroll<sec3){
+		var offset = $('#dois').offset();
+		var scroll2 = scroll - offset.top;
+		$('.fish').css('marginTop',0-scroll2/4);
+		$('.fish').css('marginLeft',0-scroll2);
+		$('.fish2').css('marginTop',0-scroll2/4);
+		$('.fish2').css('marginRight',0-scroll2);
+	}
+	/* terceiro */
+	else if(scroll<sec4){
+
+	}
+	/* quarto */
+	else if(scroll<sec5){
+
+	}
+	/* quinto */
+	else if(scroll<sec6){
+		var offset = $('#qui').offset();
+		var scroll2 = scroll - offset.top;
+		$('.img-gar').css('marginTop',100-scroll2/2);
+		rotation -= 5;
+    	$('.img-gar.um').rotate(rotation);
+    	rotation1 += 5;
+    	$('.img-gar.dois').rotate(rotation1);
+	}
+	/* sexto */
+	else if(scroll<sec7){
+		var offset = $('#quar').offset();
+		var scroll2 = scroll - offset.top;
+		$('.coffee').css('marginTop',100-scroll2/2);
+		$('.coffee').css('marginLeft',0-scroll2/2);
+		$('.coffee2').css('marginTop',0-scroll2/2);
+		$('.coffee2').css('marginRight',100-scroll2/2);
+	}
+	/* sétimo */
+	else{
+		var offset = $('#set').offset();
+		var scroll2 = scroll - offset.top;
+		$('.drop').css('marginTop',0-scroll2);
+	}
+}
+
+$(document).ready(function(){
+
+	animation();
+
+	$(window).scroll(function(){
+		animation();
+	});
+	$('#close').click(function(){
+
+		$('#menu').addClass('close');
+		$('#menu').find('article .content-center').hide();
+	   	$('#menu').animate({height: 0}, 400);
+	   	$('#open').show();
+
+	});
+	$('#open').click(function(){
+
+		$('#open').hide();
+		$('#menu').removeClass('close');
+		$('#menu').find('article .content-center').show();
+	   	 var curHeight = $('#menu').height(),
+		   	autoHeight = $('#menu').css('height', 'auto').height();
+
+		$('#menu').height(curHeight).animate({height: autoHeight}, 200);
+
+	});
+});
